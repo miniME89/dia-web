bb1e61214c45376e683cc56b077bde56394a2867
Marcel Lehwald <marcel.lehwald@googlemail.com>
2015-02-09 00:01:33 +0100

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,73 @@
+var path = require('path');
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    connect: {
+      dev: {
+        options: {
+          host: 'localhost',
+          port: 8080,
+          middleware: function (connect, options) {
+              if (!Array.isArray(options.base)) {
+                  options.base = [options.base];
+              }
+
+              // Setup the proxy
+              var middlewares = [require('grunt-connect-proxy/lib/utils').proxyRequest];
+
+              // Serve static files.
+              options.base.forEach(function(base) {
+                  middlewares.push(connect.static(base));
+              });
+
+              // Make directory browse-able.
+              var directory = options.directory || options.base[options.base.length - 1];
+              middlewares.push(connect.directory(directory));
+
+              return middlewares;
+          }
+        },
+        proxies: [
+          {
+            context: '/server',
+            host: 'localhost',
+            port: 7000,
+            changeOrigin: true,
+            rewrite: {
+              '^/server': ''
+            }
+          },
+          {
+            context: '/executor',
+            host: 'localhost',
+            port: 7001,
+            changeOrigin: true,
+            rewrite: {
+              '^/executor': ''
+            }
+          }
+        ]
+      },
+    },
+    watch: {
+      dev: {
+        files:  ['./**/*'],
+        tasks:  ['express:dev'],
+        options: {
+          spawn: false,
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-connect-proxy');
+
+  grunt.registerTask('default', function (target) {
+      grunt.task.run(['configureProxies:dev', 'connect:dev', 'watch:dev']);
+  });
+
+};
